2dec3b7e6e52435b1e6070d010d4cbd0487641ff
dantefleschpereira <48799984+dantefleschpereira@users.noreply.github.com>
2021-07-04 10:15:53 -0300

    update

--- PL/Migrations/20210704131411_Bd inicial.Designer.cs
+++ PL/Migrations/20210704131411_Bd inicial.Designer.cs
@@ -10,7 +10,7 @@ using PL;
 namespace PL.Migrations
 {
     [DbContext(typeof(SecondHandContext))]
-    [Migration("20210704114750_Bd inicial")]
+    [Migration("20210704131411_Bd inicial")]
     partial class Bdinicial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
